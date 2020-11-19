312e3f482ffb895f9b74513fb1b7ef2149be9110
Tianyu Gu, Oliver <33976851+Maitotoxin@users.noreply.github.com>
2020-11-19 16:47:12 -0500

    patient info display backend

---
+++ controller/patientInfoController.js
@@ -0,0 +1,67 @@
+const database = require('../config/databaseConfig');
+const common = require('./util/common');
+const xss = require('xss');
+
+exports.getPatientHospitalInfo = getPatientHospitalInfo;
+exports.getPatientDoctorInfo = getPatientDoctorInfo;
+exports.getPatientLabInfo = getPatientLabInfo;
+
+function getPatientHospitalInfo(req, res, next) {
+	const patient_id = xss(req.session.patient_id);
+	database.setUpDatabase(function (connection) {
+		connection.connect();
+		var sql = 'select hospital_id, hospital_name, st_address, city, state, zipcode, phone, maximun_patients from hospital';
+		connection.query(sql, [patient_id], function (err, result) {
+			if (err) {
+				console.log('[SELECT ERROR] - ', err.message);
+				res.send('SQL query error');
+				return;
+			}
+			patientHospitalInfo = result[0];
+			//console.log(userInfo);
+			res.render('patient/hospitalDisplay', {
+				patientHospitalInfo: patientHospitalInfo
+			});
+		});
+	});
+}
+
+function getPatientDoctorInfo(req, res, next) {
+	const patient_id = xss(req.session.patient_id);
+	database.setUpDatabase(function (connection) {
+		connection.connect();
+		var sql = 'select staff_no, first_name, last_name, gender, phone, description from staff where staff_class = ?';
+		connection.query(sql, ['D'], function (err, result) {
+			if (err) {
+				console.log('[SELECT ERROR] - ', err.message);
+				res.send('SQL query error');
+				return;
+			}
+			patientDoctorInfo = result[0];
+			//console.log(userInfo);
+			res.render('patient/doctorDisplay', {
+				patientDoctorInfo: patientDoctorInfo
+			});
+		});
+	});
+}
+
+function getPatientLabInfo(req, res, next) {
+	const patient_id = xss(req.session.patient_id);
+	database.setUpDatabase(function (connection) {
+		connection.connect();
+		var sql = 'select lab_id, lab_name, description from lab';
+		connection.query(sql, [patient_id], function (err, result) {
+			if (err) {
+				console.log('[SELECT ERROR] - ', err.message);
+				res.send('SQL query error');
+				return;
+			}
+			patientLabInfo = result[0];
+			//console.log(userInfo);
+			res.render('patient/labDisplay', {
+				patientLabInfo: patientLabInfo
+			});
+		});
+	});
+}
