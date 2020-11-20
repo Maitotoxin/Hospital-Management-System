0198bf6c8b608dc53ead8dcfa67469723d77a344
Tianyu Gu, Oliver <33976851+Maitotoxin@users.noreply.github.com>
2020-11-20 03:34:24 -0500

    3:34

--- middleware/loginGuard.js
+++ middleware/loginGuard.js
@@ -12,7 +12,7 @@ const guard = (req, res, next) => {
         return;
     }
     if(!didUserLogin && !didAdminLogin) {
-        if(url != '/login' && url != '/register' && url != '/admin/login' && url != '/admin/register') {
+        if(url != 'patient/login' && url != 'patient/register' ) {
             res.redirect('/error');
         } else{
             next();
@@ -20,8 +20,8 @@ const guard = (req, res, next) => {
     } else if(didUserLogin && !didAdminLogin) {
         if(url == '/admin/dashboard' || url == '/admin/logout' ||url == '/admin/adminUserDisplay' ||url == '/admin/adminPolicyDisplay' ||url == '/admin/adminPolicyRegister' ||url == '/admin/adminHomeDisplay' ||url == '/admin/adminAutoDisplay' ||url == '/admin/adminHomeInvoiceDisplay' ||url == '/admin/adminAutoInvoiceDisplay' ||url == '/admin/adminHomePayDisplay' ||url == '/admin/adminAutoPayDisplay' ||url == '/admin/adminDriverDisplay') {
             res.redirect('/admin/login');
-        } else if (url == '/login' || url == '/register'){
-            res.redirect('/dashboard');
+        } else if (url == 'patient/login' || url == 'patient/register'){
+            res.redirect('patient/dashboard');
         } else {
             next();
         }
