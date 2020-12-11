b010b2e51de346e82ac5c94aaefe0a8e43609856
wenhao guo <guowhao97@163.com>
2020-12-10 19:51:12 -0500

    1950

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ const staffICDInfoDisplay = require('./routes/staff/icdInfoDisplay');
 const staffTreatmentInfoDisplay = require('./routes/staff/treatmentInfoDisplay');
 const staffTestInfoDisplay = require('./routes/staff/testInfoDisplay');
 const staffLabInfoDisplay = require('./routes/staff/labInfoDisplay');
-const staffManageAppointment = require('./routes/staff/manageAppointment');
+//const staffManageAppointment = require('./routes/staff/manageAppointment');
 
 const staffCurePatientMedicine = require('./routes/staff/curePatientMedicine');
 //app.use('',error)
@@ -102,7 +102,7 @@ app.use('/staff/medicineInfoDisplay', staffMedicineInfoDisplay);
 app.use('/staff/treatmentInfoDisplay', staffTreatmentInfoDisplay);
 app.use('/staff/testInfoDisplay', staffTestInfoDisplay);
 app.use('/staff/labInfoDisplay', staffLabInfoDisplay);
-app.use('/staff/manageAppointment', staffManageAppointment);
+//app.use('/staff/manageAppointment', staffManageAppointment);
 
 app.use('staff/curePatientMedicine',staffCurePatientMedicine);
 
