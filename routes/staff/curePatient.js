74bebfa6f3b62fc5d43477fa61e85a397c315810
wenhao guo <guowhao97@163.com>
2020-12-11 20:19:44 -0500

    1608

--- routes/staff/curePatient.js
+++ routes/staff/curePatient.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
 const PatientMedicineInfo = require('../../controller/staffInfoController')
-//const curePatientMedicine = require('../../controller/staffCureController')
+const curePatientMedicine = require('../../controller/staffCureController')
 
 
 const curePatient = express.Router();
 
 curePatient.get('/', PatientMedicineInfo.getDoctorAppointmentInfoAccept);
 
-curePatient.post('/', PatientMedicineInfo.getPatientMedicalRecordInfo);
-
+curePatient.post('/getPatientMedicalRecordInfo', PatientMedicineInfo.getPatientMedicalRecordInfo);
+curePatient.post('/createPatientInvoice', curePatientMedicine.createPatientInvoice);
 module.exports = curePatient;
